a3200120a6c95527f781fccbf0166d90be77dc2f
Joonas Bergius <joonas.bergius@gmail.com>
2012-06-21 10:03:41 -0600

    Add missing relationships to Message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ActiveRecord::Base
-  belongs_to :dialogue
+  belongs_to :conversation
   belongs_to :sender, class_name: "User"
+  belongs_to :recipient, class_name: "User"
   attr_accessible :content, :sender_id, :recipient_id
 
   validates :content, :sender_id, presence: true
