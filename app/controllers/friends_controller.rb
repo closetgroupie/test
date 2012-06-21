7b438983054f0d829b8199c5ed2a1d92a2b90bda
Tres Spicher <tres@tspike.com>
2012-06-20 18:15:50 -0600

    incremental commit. toy REDIS caching of facebook Graph API data working

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -3,7 +3,11 @@ class FriendsController < ApplicationController
 
   def index
     if current_user.has_facebook?
-      @friends = get_friends_for_current_user
+      #@friends = get_friends_for_current_user
+      cache_current_user_friends
+      cache_facebook_members
+      #cache_facebook_profile(current_user.facebook_authentication.uid)
+      #binding.pry
     end
   end
 
--- app/helpers/friends_helper.rb
+++ app/helpers/friends_helper.rb
@@ -8,4 +8,39 @@ module FriendsHelper
     get_graph.get_connections(current_user.facebook_authentication.uid, "friends", :fields=>"name,picture")
   end
 
+  def cache_current_user_friends
+    friends = get_friends_for_current_user
+    friend_ids = Array.new
+    friends.each do |friend|
+      friend_ids += [friend['id']]
+    end
+    cache_facebook_profiles(friend_ids)
+
+    friends.each do |friend|
+      REDIS.sadd("user:facebook_friends:user_#{current_user.facebook_authentication.uid}", friend['id'])
+    end
+  end
+
+  def cache_facebook_members
+    facebook_users = Authentication.where "provider = 'facebook'"
+
+    member_facebook_ids = Array.new
+
+    facebook_users.each do |user|
+      REDIS.sadd("user:facebook_users", user.uid)
+      member_facebook_ids += [user.uid]
+    end
+
+    cache_facebook_profiles(member_facebook_ids)
+  end
+
+  def cache_facebook_profiles(uids)
+    profiles = get_graph.get_objects(uids, :fields=>"name,picture")
+    #binding.pry
+    profiles.each do |uid, profile|
+      REDIS.set("user:facebook_users:user_#{uid}:name", profile['name'])
+      REDIS.set("user:facebook_users:user_#{uid}:picture", profile['picture'])
+    end
+  end
+
 end
