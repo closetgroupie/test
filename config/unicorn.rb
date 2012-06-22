20df74e84d75dc2847169af7730fa4ec3f720184
Joonas Bergius <joonas.bergius@gmail.com>
2012-06-22 02:03:23 -0600

    Instead of using the PG connection established by the master, recreate a new connection per worker

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -10,3 +10,13 @@ worker_processes 2
 preload_app true
 
 timeout 30
+
+
+# See: http://unicorn.bogomips.org/examples/unicorn.conf.rb
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
