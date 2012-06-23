110b6b3df94917a21d03d4aba642181e8e0ea16e
Joonas Bergius <joonas.bergius@gmail.com>
2012-06-23 15:59:57 -0600

    Initial "add to favorites" implementation, replaced jquery.tipsy with twitter bootstrap tooltips.

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,9 +1,11 @@
 class ActivitiesController < ApplicationController
   def index
+    @favorites = get_user_favorites()
     @activities = Activity.includes(:user, :entity => [:hero_image]).limit(40)
   end
 
   def feed
+    @favorites = get_user_favorites()
     if params.has_key?(:since)
       @activities = Activity.includes(:user, :entity => [:hero_image]).since(params[:since].to_i).limit(40)
     elsif params.has_key?(:before)
@@ -18,5 +20,11 @@ class ActivitiesController < ApplicationController
   def previous
     @activities = Activity.since_id(params[:id].to_s)
     render "previous", :layout => false
+  end
+
+  private
+
+  def get_user_favorites()
+    return current_user ? current_user.favorites.pluck(:item_id) : []
   end
 end
