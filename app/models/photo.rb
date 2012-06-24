0ed342c5486d23f87c328d8e9ac576c99cf5d032
Tres Spicher <tres@tspike.com>
2012-06-23 19:08:53 -0600

    photo rotation works, had to recreate_versions after save

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,4 +3,14 @@ class Photo < ActiveRecord::Base
   attr_accessible :image, :item_id, :ordering
 
   mount_uploader :image, ImageUploader
+
+  def rotate!(degrees)
+    image.manipulate! do |photo|
+      photo.rotate degrees.to_s
+      photo = yield(photo) if block_given?
+      photo
+    end
+    save!
+    image.recreate_versions!
+  end
 end
