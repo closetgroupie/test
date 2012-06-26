fbceacd5f6868f7df0db283e912344857a2540a9
Joonas Bergius <joonas.bergius@gmail.com>
2012-06-26 17:57:13 -0600

    Temporary solution until we get baby boys + baby girls consolidated

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,8 +1,17 @@
 class LandingController < ApplicationController
   def index
-    womens = [751, 747, 637, 161]
-    girls  = [620, 376, 375, 540]
-    boys   = [392, 429, 364, 550]
-    babies = [433, 98, 15, 331]
-    @items_by_segment = Item.includes(:hero_image, :size, :segment).where(id: [womens, girls, boys, babies].flatten).group_by(&:segment)
+    @items_by_segment = {
+      "Womens" => [751, 747, 637, 161],
+      "Girls"  => [620, 376, 375, 540],
+      "Boys"   => [392, 429, 364, 550],
+      "Babies" => [433, 98, 15, 331]
+    }
+
+    @items_by_segment.each do |segment, item_ids|
+      @items_by_segment[segment] = Item.includes(:hero_image, :size, :segment).where(id: item_ids)
+    end
+
+    # TODO: Use this instead once we have consolidated baby girls + baby boys in to one
+    # @items_by_segment = Item.includes(:hero_image, :size, :segment).where(id: [womens, girls, boys, babies].flatten).group_by(&:segment)
+  end
 end
