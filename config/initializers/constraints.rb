519b509351a54f646fe9ecb49a80295edd430b7a
Joonas Bergius <joonas.bergius@gmail.com>
2012-06-25 23:20:15 -0600

    Moved shop.rb constants/constraints to appropriate files

---
+++ config/initializers/constraints.rb
@@ -0,0 +1,29 @@
+class AdminConstraint
+  def self.matches?(request)
+    # TODO: Is this good/secure enough, or should we go all out here?
+    if request.session and request.session[:user_id]
+      user = User.find(request.session[:user_id])
+      return (user.present? and user.admin?)
+    else
+      return false
+    end
+  end
+end
+
+class AuthenticatedConstraint
+  def self.matches?(request)
+    # TODO: Is this good/secure enough, or should we go all out here?
+    if request.session and request.session[:user_id]
+      user = User.find(request.session[:user_id])
+      return user.present?
+    else
+      return false
+    end
+  end
+end
+
+class SegmentConstraint
+  def self.matches?(request)
+    SHOPPING_SEGMENTS.include?(request.params[:segment].downcase)
+  end
+end
