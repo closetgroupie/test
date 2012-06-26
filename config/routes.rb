c950997ff8f39a4a77d7bc4b5caf924134266810
Joonas Bergius <joonas.bergius@gmail.com>
2012-06-25 18:55:04 -0600

    Capture the urls with non-essential slugs

--- config/routes.rb
+++ config/routes.rb
@@ -118,6 +118,7 @@ Closetgroupie::Application.routes.draw do
 
   # Legacy URLs
   get "item-details/:legacy_id" => "legacy_redirects#item_details"
+  get "item-details/:legacy_id/:dontcare" => "legacy_redirects#item_details"
   get "closet/:legacy_id"       => "legacy_redirects#closet"
   get "legacy/image/:item/:image" => "legacy_redirects#item_image"
 
