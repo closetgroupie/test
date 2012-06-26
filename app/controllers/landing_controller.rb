058cf499272b47f7ead946b3a1c4f35d77ba83e7
Joonas Bergius <joonas.bergius@gmail.com>
2012-06-26 17:32:33 -0600

    Rely on iteration

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,4 +1,8 @@
 class LandingController < ApplicationController
   def index
-  end
+    womens = [751, 747, 637, 161]
+    girls  = [620, 376, 375, 540]
+    boys   = [392, 429, 364, 550]
+    babies = [433, 98, 15, 331]
+    @items_by_segment = Item.includes(:hero_image, :size, :segment).where(id: [womens, girls, boys, babies].flatten).group_by(&:segment)
 end
