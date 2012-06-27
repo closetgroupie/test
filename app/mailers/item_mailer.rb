056a31dd18479f8be0e9265cc0bb908690a6fd79
Tres Spicher <tres@tspike.com>
2012-06-27 01:03:51 -0600

    basic email to groupie on new item added works. template needs a lot of work.

--- app/mailers/item_mailer.rb
+++ app/mailers/item_mailer.rb
@@ -1,3 +1,10 @@
 class ItemMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "ClosetGroupie.com <no-reply@closetgroupie.com>"
+  layout "email"
+
+  def item_of_interest_email(item, groupie)
+    @item, @groupie = item, groupie
+    mail(to: groupie.email,
+         subject: "#{@item.user.name} listed #{@item.title} on ClosetGroupie!")
+  end
 end
