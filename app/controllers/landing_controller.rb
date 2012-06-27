465fc652ed134016cf71c50ec1f1b28fd431cba9
Joonas Bergius <joonas.bergius@gmail.com>
2012-06-26 23:22:54 -0600

    Backup of latest work, just in case...

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -3,12 +3,17 @@ class LandingController < ApplicationController
     @items_by_segment = {
       "Womens" => [751, 747, 637, 161],
       "Girls"  => [620, 376, 375, 540],
-      "Boys"   => [392, 429, 364, 550],
+      "Boys"   => [550, 392, 429, 364],
       "Babies" => [433, 98, 15, 331]
     }
 
+    # Boys: White pants, Blue Shirt, Pants, green shirt
+    # Womens: Print dress, Shirt, Dress, Shoes
+    # Girls: Outfit, Shirt, Skirt, Shoes
+    # Babies: dress, purple thing, Lion thing, Romper
+
     @items_by_segment.each do |segment, item_ids|
-      @items_by_segment[segment] = Item.includes(:hero_image, :size, :segment).where(id: item_ids)
+      @items_by_segment[segment] = Item.includes(:hero_image, :size, :segment).unscoped.where(id: item_ids)
     end
 
     # TODO: Use this instead once we have consolidated baby girls + baby boys in to one
