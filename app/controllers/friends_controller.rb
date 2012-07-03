0801a98d3c8c1bf67dbb2ce2d5a1198451494762
Joonas Bergius <joonas.bergius@gmail.com>
2012-07-03 09:48:34 -0600

    Fix the exception that was raised when someone without logging in tried to access /friends

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,8 +1,10 @@
 class FriendsController < ApplicationController
   include FriendsHelper
 
+  before_filter :require_login
+
   def index
-    if current_user.has_facebook?
+    if current_user and current_user.has_facebook?
       @friends = get_friends_using_site(current_user.facebook_authentication.uid)
     end
   end
