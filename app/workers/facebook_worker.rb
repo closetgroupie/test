170ca0e4f8fdda4d3da2d5b8af8aa0c6e9238341
Tres Spicher <tres@tspike.com>
2012-07-02 18:21:20 -0600

    first pass of favoriting items getting posted on FB complete

--- app/workers/facebook_worker.rb
+++ app/workers/facebook_worker.rb
@@ -1,10 +1,9 @@
 class FacebookWorker
-  #include Sidekiq::Worker
+  include Sidekiq::Worker
   include FacebookHelper
   include Rails.application.routes.url_helpers
 
   def perform(action, param)
-    puts action.class, action, param
     case action
     when 'add_favorite'
       add_favorite(param)
@@ -12,12 +11,11 @@ class FacebookWorker
   end
 
   def add_favorite(param)
-    u = User.find(param[:user])
-    i = Item.find(param[:item])
+    u = User.find(param['user'])
+    i = Item.find(param['item'])
     if u.has_facebook?
       g = get_graph(u)
-      binding.pry
-      g.put_connections("me", "closetgroupie-dev:Favorite", :object => item_url(i, :host => Rails.application.config.root_url))
+      g.put_connections("me", "closetgroupie-dev:Favorite", :item => item_url(i, :host => Rails.application.config.root_url))
     end
   end
 end
