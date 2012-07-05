20409fba636b4f5768c77b91a97d30a01f4a3b5c
Joonas Bergius <joonas.bergius@gmail.com>
2012-07-04 22:00:54 -0600

    Environment based base url

--- app/lib/api/entities/user.rb
+++ app/lib/api/entities/user.rb
@@ -4,7 +4,11 @@ module Api
       expose :name
       expose :email
       expose :id
-      expose :avatar_url
+      expose :avatar_url do | user , options |
+        # TODO: Come up with a better implementation.
+        base = Rails.env.production? ? 'https://closetgroupie.com' : 'http://localhost:3000'
+        "#{base}#{user.avatar_url}"
+      end
       expose( :api_key ){ "abc123" }
     end
   end
