ac99390358a279183b17c498ef5187b87cf15f36
Tres Spicher <tres@tspike.com>
2012-07-05 16:48:09 -0600

    forgot to add facebook configuration

---
+++ config/initializers/facebook.rb
@@ -0,0 +1,5 @@
+Rails.configuration.publish_new_item_listings = false
+Rails.configuration.publish_follows = true
+Rails.configuration.publish_sales = true
+Rails.configuration.publish_purchases = false
+Rails.configuration.publish_favorites = true
