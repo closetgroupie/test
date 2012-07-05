557e69fc2c2b194305a504af1b4e672819d39a68
Joonas Bergius <joonas.bergius@gmail.com>
2012-07-04 18:48:36 -0600

    Change landing page items out

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,10 +1,10 @@
 class LandingController < ApplicationController
   def index
     ids_by_segment = {
-      "Women" => [637, 751, 747, 161],
-      "Girls"  => [620, 375, 376, 540],
+      "Women"  => [439, 862, 139, 164],
+      "Girls"  => [618, 492, 259, 240],
       "Boys"   => [550, 392, 429, 364],
-      "Babies" => [433, 98, 331, 15]
+      "Babies" => [360, 295, 289, 292]
     }
 
     items = Item.unscoped.includes(:hero_image, :size).find(ids_by_segment.values.flatten).index_by(&:id)
