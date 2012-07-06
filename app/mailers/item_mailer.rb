a805f812b67ecf9b0c594f06f9aeeb8ecdc0637c
Tres Spicher <tres@tspike.com>
2012-07-06 11:06:12 -0600

    minor tweaks to new item email

--- app/mailers/item_mailer.rb
+++ app/mailers/item_mailer.rb
@@ -4,17 +4,11 @@ class ItemMailer < ActionMailer::Base
   default from: "ClosetGroupie.com <no-reply@closetgroupie.com>"
   layout "email"
 
-  def item_of_interest_email(item, groupie)
-    @item, @groupie = item, groupie
-    mail(to: groupie.email,
+  def item_of_interest_email(item_id, groupie_id)
+    @item = Item.find(item_id)
+    @groupie = User.find(groupie_id)
+    mail(to: @groupie.email,
          subject: "#{@item.user.name} listed #{@item.title} on ClosetGroupie!")
   end
 
-  def new_item_email(item_id, groupie_id)
-    @groupie = User.find(groupie_id)
-    @item    = Item.includes(:user).find(item_id)
-    @user    = @item.user
-    mail(to: @groupie.email,
-         subject: "#{@user.name} posted a new item on ClosetGroupie")
-  end
 end
