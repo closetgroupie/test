f004450ad7f66a60ceeda46653904c7ed1a15c71
Joonas Bergius <joonas.bergius@gmail.com>
2012-07-07 02:40:10 -0600

    Use credentials from the environment configuration

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,13 +4,8 @@ end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   # TODO: Move these in to ENVironment variables
-  if Rails.env.development?
-    facebook_key = "241246552660833"
-    facebook_secret = "b916fb1ecf5800938015fab4e2fb93e2"
-  else
-    facebook_key = "195906460469934"
-    facebook_secret = "3c11385cf4f9036acb5acd8274028d80"
-  end
+  facebook_key = Closetgroupie::Application.config.facebook[:key]
+  facebook_secret = Closetgroupie::Application.config.facebook[:secret]
 
   # provider :identity, :model => User, :on_failed_registration => lambda { |env|
   #          SessionsController.action(:new).call(env)
