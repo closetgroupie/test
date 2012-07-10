9c9dbbca46b50e1e99ebf3dc18ac32e70fdae815
Tres Spicher <tres@tspike.com>
2012-07-10 17:01:54 -0600

    remove unnecessary worker

--- app/mailers/review_mailer.rb
+++ app/mailers/review_mailer.rb
@@ -4,9 +4,9 @@ class ReviewMailer < ActionMailer::Base
   default from: "ClosetGroupie.com <no-reply@closetgroupie.com>"
   layout "email"
 
-  def review_reminder_email(buyer_id, seller_id)
+  def review_reminder_email(buyer_id, closet_id)
     @buyer = User.find(buyer_id)
-    @seller = User.find(seller_id)
+    @seller = User.find_by_closet_id(closet_id)
     @orders = Order.where(:buyer_id => buyer_id, :seller_id => seller_id)
     mail(to: @buyer.email, subject: "Leave feedback for #{seller.name} on ClosetGroupie")
   end
