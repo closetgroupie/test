e79741c60f87be39420e9a4762d2424624ddf7ee
Tres Spicher <tres@tspike.com>
2012-07-10 17:01:54 -0600

    remove unnecessary worker

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -62,7 +62,7 @@ class Cart < ActiveRecord::Base
     end
     orders.each do |order|
       OrderMailer.delay.sale_made_email(order.id)
-      ReminderSchedulingWorker.perform_async(order.id) # Schedule review reminder
+      ReminderDeliveryWorker.perform_in(7.days, order.id) # Schedule review reminder
     end
     update_attribute(:purchased_at, purchased_at)
     save
--- app/workers/reminder_delivery_worker.rb
+++ app/workers/reminder_delivery_worker.rb
@@ -6,9 +6,9 @@ class ReminderDeliveryWorker
 
   def perform(order_id)
     o = Order.find(order_id)
-    seller = User.find(o.seller_id)
+    closet_id = Order.items.last.closet_id
     if Review.made_by_buyer_for_closet(o.buyer_id, seller.closet_id).count == 0
-      ReviewMailer.delay.review_reminder_email(o.seller_id, o.buyer_id)
+      ReviewMailer.review_reminder_email(o.seller_id, o.buyer_id).deliver
     end
   end
 end
