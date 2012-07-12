437243379868734a28a3bce0fc8be62f347460e3
Joonas Bergius <joonas.bergius@gmail.com>
2012-07-11 21:16:05 -0600

    Appropriate validations and styling for the form

--- app/assets/javascripts/become-a-curator.js
+++ app/assets/javascripts/become-a-curator.js
@@ -1,24 +1,29 @@
 //= require rails.validations
 //= require_self
+
+var validation_elements = $('#new_curator_application').children('[data-validate="true"]').map(function(idx, el) {
+        return $(el);
+    }),
+    $submit = $('#submit_application');
+
 clientSideValidations.callbacks.element.fail = function(element, message, callback) {
     callback();
+
     if (element.data('valid') !== false) {
-    //     console.log('invalid');
-        var title_msg = element.attr('placeholder') + " " + message;
-        element.attr('title', title_msg);
-        element.tooltip({ trigger: "focus" }).tooltip('show');
-    //     // element.parent().find('.message').hide().show('slide', {direction: "left", easing: "easeOutBounce"}, 500);
+        if (element.data('tooltip') && element.data('tooltip').enabled == false) {
+            element.tooltip('enable').tooltip('show');
+        } else if (element.data('tooltip')) {
+            element.tooltip('show');
+        } else {
+            var title_msg = element.attr('placeholder') + " " + message;
+            element.tooltip({
+                trigger: "focus",
+                title: title_msg
+            });
+        }
     }
 };
 
-clientSideValidations.callbacks.element.pass = function(element, callback) {
-    // Take note how we're passing the callback to the hide() 
-    // method so it is run after the animation is complete.
-    callback();
-    element.tooltip('disable');
-    // element.parent().find('.message').hide('slide', {direction: "left"}, 500, callback);
+clientSideValidations.callbacks.form.fail = function(form, eventData) {
+    $('#submit_application').tooltip({ title: "Please correct the errors in the fields marked with red border before submitting" }).tooltip('show');
 };
-
-// clientSideValidations.callback.form.pass = function(form, eventData) {
-//     $('#submit_application').attr('disabled', 'disabled');
-// };
