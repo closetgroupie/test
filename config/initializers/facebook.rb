816337b4b67c259d0f02795898dfeb59df62e83e
Joonas Bergius <joonas.bergius@gmail.com>
2012-07-13 14:39:52 -0600

    Only publish favorites until we get the rest approved

--- config/initializers/facebook.rb
+++ config/initializers/facebook.rb
@@ -1,5 +1,5 @@
 Rails.configuration.publish_new_item_listings = false
-Rails.configuration.publish_follows = true
-Rails.configuration.publish_sales = true
+Rails.configuration.publish_follows = false
+Rails.configuration.publish_sales = false
 Rails.configuration.publish_purchases = false
 Rails.configuration.publish_favorites = true
