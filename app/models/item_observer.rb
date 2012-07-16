e2f5a12df164729744d031e84618455c8a94cb18
Joonas Bergius <joonas.bergius@gmail.com>
2012-07-16 09:51:06 -0600

    Rename the new item e-mail to match the templates/delay

--- app/models/item_observer.rb
+++ app/models/item_observer.rb
@@ -1,7 +1,7 @@
 class ItemObserver < ActiveRecord::Observer
   def after_create(model)
     model.user.groupies.each do |groupie| # har har
-      ItemMailer.delay.new_item_email(model.id, groupie.id)
+      ItemMailer.delay.item_of_interest_email(model.id, groupie.id)
     end
 
     FacebookWorker.perform_async('sell_item', {'user' => model.user.id, 'item' => model.id})
